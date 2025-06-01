665ba12b8a6504e9ec2cc503570f9550d3d3dc0c
Jacob Brown <102562306+Jaco-bB-rown@users.noreply.github.com>
2025-05-31 23:09:54 -0700

    moved country vetorization to our_datasets

--- our_datasets.py
+++ our_datasets.py
@@ -2,7 +2,7 @@ from torch.utils.data import Dataset
 import torch, torchvision
 import os
 import pandas as pd
-from Country_dict import country_dict
+#from Country_dict import comp_country_dict
 from torchvision.transforms.v2 import Lambda
 class Country_images(Dataset):
     # csv: path to csv with annotations
@@ -12,7 +12,8 @@ class Country_images(Dataset):
         self.labels = pd.read_csv(csv)
         self.root_dir = root_dir
         self.transform = transform#torchvision.transforms.Compose([ResNet152_Weights.IMAGENET1K_V2.transforms()])
-        self.num_classes = len(country_dict)
+        self.country_dict = {item: index for index, item in enumerate(self.labels["country"].unique())}
+        self.num_classes = self.get_num_classes()
         #self.target_transform = Lambda(lambda y: torch.zeros(num_classes, dtype=torch.float).scatter_(dim=0, index=torch.tensor(y,dtype=torch.int64), value=1))
     def __len__(self):
         return(len(self.labels))
@@ -21,16 +22,17 @@ class Country_images(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
         # isolating single image
-        img_path = os.path.join(self.root_dir,self.labels.iloc[idx, 1]+"/"+self.labels.iloc[idx, 0])
+        img_path = os.path.join(os.path.join(self.root_dir,self.labels.iloc[idx, 1]),self.labels.iloc[idx, 0])
         #print(img_path)
         image = self.transform(torchvision.io.decode_image(img_path))
         # save specific image label
         #label = self.labels.iloc[idx, 1:]
-        label = target_transform(country_dict[self.labels.iloc[idx, 1]],self.num_classes)
+        label = self.target_transform(self.country_dict[self.labels.iloc[idx, 1]])
         #print(label)
         return image, label
 
+    def get_num_classes(self):
+        return len(self.country_dict)
         
-        
-def target_transform(y, num_classes):
-        return torch.zeros(num_classes, dtype=torch.float).scatter_(dim=0, index=torch.tensor(y,dtype=torch.int64), value=1)
+    def target_transform(self,y):
+        return torch.zeros(self.num_classes, dtype=torch.float).scatter_(dim=0, index=torch.tensor(y,dtype=torch.int64), value=1)
